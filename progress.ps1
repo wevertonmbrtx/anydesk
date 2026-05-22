fe00d0a0ad8c5e7164d7be0031cf0060fd638781
Code_4X <wevertonmbrt@gmail.com>
2026-05-22 01:41:20 -0300

    Refactor Draw-Bar function in progress.ps1 to enhance loading bar display with improved character representation

--- progress.ps1
+++ progress.ps1
@@ -1,4 +1,4 @@
-param(
+﻿param(
     [ValidateSet('installed', 'portable', 'auto')]
     [string]$Mode = 'auto'
 )
@@ -51,7 +51,7 @@ function Draw-Bar([int]$p, [string]$status = '') {
         [Console]::SetCursorPosition(0, 0)
         [Console]::Write(('Loading AnyDesk...').PadRight($windowCols))
         [Console]::SetCursorPosition(0, 1)
-        [Console]::Write('[' + ('0' * $f) + (' ' * $e) + ']' + "$p%".PadLeft(5))
+        [Console]::Write('[' + ('█' * $f) + ('░' * $e) + ']' + "$p%".PadLeft(5))
         [Console]::SetCursorPosition(0, 2)
         [Console]::Write($status.PadRight($windowCols))
     } catch {}
